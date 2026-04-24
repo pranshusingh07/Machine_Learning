9953d4d5854cdfea9cc4920ded29bd8e639a5f94
Pranshu Singh <chauhanpranshu521@gmail.com>
2026-04-24 23:44:27 +0530

    Create svm.py

---
+++ svm.py
@@ -0,0 +1,22 @@
+# Support Vector Machine (SVM)
+
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Dataset
+X = [[1],[2],[3],[4],[5],[6]]
+y = [0,0,0,1,1,1]
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Model
+model = svm.SVC(kernel='linear')
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+print("Predictions:", y_pred)
+print("Accuracy:", accuracy_score(y_test, y_pred))
